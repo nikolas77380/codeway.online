88423f04bfa6dd1eda1a8c74cfaf91252834028f
DenGolivets <oxytechmusic@gmail.com>
2024-07-09 16:37:56 +0300

    [cw-25]: add video section

---
+++ src/mocks/VideoItem/helper.ts
@@ -0,0 +1,2 @@
+export const URL_VIDEO_YOUTUBE = 'https://www.youtube.com/embed/8Kmd4Jx9gb0?si=U_LQs1EwZFz2kyVa'
+export const URL_IMG_YOUTUBE = 'https://img.youtube.com/vi/8Kmd4Jx9gb0/maxresdefault.jpg'
