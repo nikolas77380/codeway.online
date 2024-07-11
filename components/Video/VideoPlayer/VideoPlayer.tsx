fa9fed51176b2c9121c1b321ef4d14374ab4b460
nikolas77380 <mykola.kipniak@fishbrain.com>
2024-07-11 22:04:36 +0300

    refactor code

--- components/Video/VideoPlayer/VideoPlayer.tsx
+++ components/Video/VideoPlayer/VideoPlayer.tsx
@@ -11,22 +11,17 @@ interface VideoPlayerProps {
   autoplay: boolean;
 }
 
-const VideoPlayer = ({ onClose, autoplay }: VideoPlayerProps) => {
-  const autoplayParam = autoplay ? "1" : "0";
-
-  return (
-    <Box sx={style.videoWrapper}>
-      <IconButton onClick={onClose} sx={style.closeButtonModal}>
-        <CloseIcon fontSize="medium" sx={style.closeIconModal} />
-      </IconButton>
-      <iframe
-        src={`${URL_VIDEO_YOUTUBE}&autoplay=${autoplayParam}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </Box>
-  );
-};
+const VideoPlayer = ({ onClose, autoplay }: VideoPlayerProps) => (
+  <Box sx={style.videoWrapper}>
+    <IconButton onClick={onClose} sx={style.closeButtonModal}>
+      <CloseIcon fontSize="medium" sx={style.closeIconModal} />
+    </IconButton>
+    <iframe
+      src={`${URL_VIDEO_YOUTUBE}&autoplay=${Number(autoplay)}`}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </Box>
+);
 
 export default VideoPlayer;
