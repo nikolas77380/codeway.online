a0b57c6b04db1e18c8d2ac0e2e9a9e667486a97d
cepuii <54589413+cepuii@users.noreply.github.com>
2024-07-16 10:22:56 +0300

    [cw-3]: Remove duplicated import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ChooseUs from "@/components/ChooseUs/ChooseUs";
 import Faq from "@/components/Faq/Faq";
+import Testimonials from "@/components/Testimonials/Testimonials";
 import Video from "@/components/Video/Video";
 import ContactUs from "@/components/contactUs/ContactUs";
 import CookieDialog from "@/components/cookieDialog/CookieDialog";
@@ -7,8 +8,6 @@ import CoursesList from "@/components/coursesListBlock/CoursesList/CoursesList";
 import Header from "@/components/header/Header";
 import { Box } from "@mui/material";
 import style from "./page.style";
-import Faq from "@/components/Faq/Faq";
-import Testimonials from "@/components/Testimonials/Testimonials";
 
 export default function Home() {
   return (
