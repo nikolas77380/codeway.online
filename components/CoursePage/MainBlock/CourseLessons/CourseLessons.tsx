47448ce4e5f317c017a8a059f74265a1ea9c147b
DenGolivets <oxytechmusic@gmail.com>
2024-07-19 02:45:46 +0300

    [cw-20]: add finish course lessons item

--- components/CoursePage/MainBlock/CourseLessons/CourseLessons.tsx
+++ components/CoursePage/MainBlock/CourseLessons/CourseLessons.tsx
@@ -1,27 +1,92 @@
+'use client';
+
 import { TCourseInfo } from '@/src/mocks/mocks';
-import { Box, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+import style from './../MainBlock.style'
+import { useState } from 'react';
 
 interface CourseLessonsProps {
   lessons: TCourseInfo['lessons'];
-}
+};
 
 const CourseLessons = ({ lessons }: CourseLessonsProps) => {
 
-  
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
-    <Box>
+    <Box sx={style.mainCourseLessonsContainer}>
+      <Typography variant='h5' component='span'>  
+        Course Lessons
+      </Typography>
       {lessons.map((lesson, index) => (
-        <Box key={index}>
-          {lesson.lessonTopic}
-          <Box>
-            {lesson.lessonsList.map(list => (
-              <Box key={list.id}>
-                {list.lessonName}
-                {list.lessonDescription}
-              </Box>
-            ) )}
-          </Box>
+        <Box key={index} sx={style.accordionMainContainer}>
+          <Accordion 
+            sx={style.accordion}
+            expanded={expanded === `panel${index}`}
+            onChange={handleChange(`panel${index}`)}
+          >
+            <AccordionSummary
+              expandIcon={
+                <Box sx={{
+                  ...style.accordionArrowContainer,
+                  backgroundColor: expanded === `panel${index}` ? '#fff' : '#a855f7',
+                }}>
+                  <KeyboardArrowDownIcon sx={{
+                    ...style.accordionArrowColor,
+                    color: expanded === `panel${index}` ? '#a855f7' : '#fff',
+                  }} />
+                </Box>
+              }
+              aria-controls={`panel${index}-content`}
+              id={`panel${index}-header`}
+              sx={style.accordionSummary}
+            >
+              <Typography component='span'>
+                {lesson.lessonTopic}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={style.accordionDetails}>
+            <Typography variant='body1' component='p'>
+              {lesson.lessonsList.map(list => (
+                <Box key={list.id} sx={style.listLessonsContainer}>
+                  <Accordion sx={style.accordionList}>
+                    <AccordionSummary
+                      expandIcon={
+                        <Box sx={{
+                          ...style.accordionArrowContainer,
+                          backgroundColor: expanded === `panel${index}` ? '#fff' : '#a855f7',
+                        }}>
+                          <KeyboardArrowDownIcon sx={{
+                            ...style.accordionArrowColor,
+                            color: expanded === `panel${index}` ? '#a855f7' : '#fff',
+                          }} />
+                        </Box>
+                      }
+                      aria-controls="panel1-content"
+                      id="panel1-header"
+                      sx={style.accordionSummaryList}
+                    >
+                      <Typography component='span'>
+                        {list.lessonName}
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails sx={style.accordionDetailsList}>
+                    <Typography component='p'>
+                      {list.lessonDescription}
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                </Box>
+              ))}
+            </Typography>
+          </AccordionDetails>
+          </Accordion>
         </Box>
       ))}
     </Box>
--- components/CoursePage/MainBlock/MainBlock.style.ts
+++ components/CoursePage/MainBlock/MainBlock.style.ts
@@ -73,6 +73,177 @@ const style: { [key: string]: SxProps<Theme> } = {
       fontSize: '18px'
     },
   },
+
+  mainCourseLessonsContainer: {
+    width: 1,
+    height: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    '& span': {
+      color: '#fff',
+      fontSize: '1.8rem',
+      fontWeight: '500',
+      marginBottom: '10px',
+    },
+  },
+
+  accordionMainContainer: {
+    width: 1,
+    height: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  accordion: {
+    backgroundColor: '#1c1e2a',
+    borderRadius: '10px',
+    marginBottom: '10px',
+    marginTop: '10px',
+    overflow: 'hidden',
+    '&:first-of-type': {
+      borderTopLeftRadius: '10px',
+      borderTopRightRadius: '10px',
+    },
+    '&:last-of-type': {
+      borderBottomLeftRadius: '10px',
+      borderBottomRightRadius: '10px',
+    },
+    '&.Mui-expanded': {
+      backgroundColor: '#191b26',
+      margin: 'auto',
+    },
+    '&.Mui-expanded:first-of-type': {
+      borderTopLeftRadius: '10px',
+      borderTopRightRadius: '10px',
+    },
+    '&.Mui-expanded:last-of-type': {
+      borderBottomLeftRadius: '10px',
+      borderBottomRightRadius: '10px',
+    },
+    '& span': {
+      color: '#fff',
+      fontSize: '1.2rem',
+      fontWeight: '500',
+      '@media (max-width: 600px)': {
+        fontSize: '1rem',
+      },
+    },
+  },
+
+  accordionSummary: {
+    height: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+    '&.Mui-expanded': {
+      backgroundColor: '#9f46f1',
+    },
+  },
+
+  accordionSummaryList: {
+    height: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+    '&.Mui-expanded': {
+      backgroundColor: '#9f46f1',
+    },
+    '& span': {
+      fontSize: '16px',
+      fontWeight: '300',
+    },
+  },
+
+  accordionDetails: {
+    overflow: 'hidden',
+    height: 'auto',
+    borderBottomLeftRadius: '10px',
+    borderBottomRightRadius: '10px',
+    p: '1rem 1rem',
+    '& p': {
+      color: '#fff',
+      fontSize: '16px',
+      fontWeight: '400',
+      '@media (max-width: 600px)': {
+        fontSize: '14px',
+      },
+    },
+  },
+
+  accordionDetailsList  : {
+    overflow: 'hidden',
+    height: 'auto',
+    borderBottomLeftRadius: '10px',
+    borderBottomRightRadius: '10px',
+    p: '1rem 1rem',
+    '& p': {
+      color: '#fff',
+      fontSize: '16px',
+      fontWeight: '400',
+      '@media (max-width: 600px)': {
+        fontSize: '14px',
+      },
+    },
+  },
+
+  listLessonsContainer: {
+    paddingTop: '1rem',
+  },
+
+  accordionList: {
+    backgroundColor: '#9f46f1',
+    borderRadius: '10px',
+    marginBottom: '10px',
+    overflow: 'hidden',
+    '&:first-of-type': {
+      borderTopLeftRadius: '10px',
+      borderTopRightRadius: '10px',
+    },
+    '&:last-of-type': {
+      borderBottomLeftRadius: '10px',
+      borderBottomRightRadius: '10px',
+    },
+    '&.Mui-expanded': {
+      backgroundColor: '#191b26',
+      margin: 'auto',
+    },
+    '&.Mui-expanded:first-of-type': {
+      borderTopLeftRadius: '10px',
+      borderTopRightRadius: '10px',
+    },
+    '&.Mui-expanded:last-of-type': {
+      borderBottomLeftRadius: '10px',
+      borderBottomRightRadius: '10px',
+    },
+    '& span': {
+      color: '#fff',
+      fontSize: '1.2rem',
+      fontWeight: '500',
+      '@media (max-width: 600px)': {
+        fontSize: '1rem',
+      },
+    },
+  },
+
+  accordionArrowContainer: {
+    backgroundColor: '#a855f7',
+    borderRadius: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '&.Mui-expanded': {
+      backgroundColor: '#ffffff',
+    },
+  },
+
+  accordionArrowColor: {
+    color: '#fff',
+    fontSize: '30px',
+    '@media (max-width: 600px)': {
+      fontSize: '20px',
+    },
+    '&.Mui-expanded': {
+      color: '#a855f7', 
+    },
+  },
 };
 
 export default style;
