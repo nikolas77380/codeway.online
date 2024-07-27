a05a13ad39cbe6d7c18a7e3b9ff4c8218fe934a7
DenGolivets <oxytechmusic@gmail.com>
2024-07-27 21:18:21 +0300

    [cw-28]: add google map also install react-leaflet leaflet @types/leaflet

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -3,12 +3,14 @@ import { Box } from '@mui/material';
 import style from './ContactUsPage.style';
 import ContactUsHeader from '@/components/ContactUsPage/ContactUsHeader/ContactUsHeader';
 import GetInTouch from '@/components/ContactUsPage/GetInTouch/GetInTouch';
+import GoogleMap from '@/components/ContactUsPage/GoogleMap/GoogleMap';
 
 const ContactUsPage = () => {
   return (
     <Box sx={style.mainPageContainer}>
       <ContactUsHeader />
       <GetInTouch />
+      <GoogleMap />
     </Box>
   )
 }
---
+++ components/ContactUsPage/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Box } from '@mui/material';
+
+import { useRef, MutableRefObject } from 'react';
+
+import { MapContainer, TileLayer, Marker, useMapEvent, Popup, Tooltip } from 'react-leaflet';
+
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import style from './GoogleMap.style';
+
+const redIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+const GoogleMap = () => { 
+
+  const animateRef = useRef(false);
+
+  const lat = 50.4501;
+  const lng = 30.5234;
+  const address = "Beresteysky prospect, 9/47";
+
+  function SetViewOnClick({ animateRef }: { animateRef: MutableRefObject<boolean> }) {
+    const map = useMapEvent('click', (e) => {
+      map.setView(e.latlng, map.getZoom(), {
+        animate: animateRef.current || false,
+      })
+    })
+  
+    return null
+  };
+
+  return (
+    <Box sx={style.mapContainer}>
+      <MapContainer
+        center={[lat, lng]}
+        zoom={16}
+        scrollWheelZoom={false}
+        className='map-container'
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lng]} icon={redIcon}>
+          <Popup>
+            {address}
+          </Popup>
+          <Tooltip>
+            {address}
+          </Tooltip>
+        </Marker>
+        <SetViewOnClick animateRef={animateRef} />
+      </MapContainer>
+    </Box>
+  )
+}
+
+export default GoogleMap;
