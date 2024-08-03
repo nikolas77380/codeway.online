7261c9223f83f107a08780dae17efd7ba8b25eab
DenGolivets <oxytechmusic@gmail.com>
2024-08-03 21:15:36 +0300

    [cw-30] merge new dev

--- components/Testimonials/TestimonialCard/TestimonialCard.tsx
+++ components/Testimonials/TestimonialCard/TestimonialCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { dataTestimonialCard } from "@/src/mocks/TestimonialSection/dataTestimonialCard";
 
