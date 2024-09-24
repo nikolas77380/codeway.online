7ae1ae00af5a41f4b18cb3b4f144fe954435ff8d
cepuii <54589413+cepuii@users.noreply.github.com>
2024-09-24 16:52:45 +0300

    change nodemailer setting for new email

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -9,16 +9,9 @@ export async function POST(request: NextRequest) {
   const { email, subject, message, recipient } = await request.json();
 
   const transport = nodemailer.createTransport({
-    service: "gmail",
-    /* 
-      setting service as 'gmail' is same as providing these setings:
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true
-      If you want to use a different email provider other than gmail, you need to provide these manually.
-      Or you can go use these well known services and their settings at
-      https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
-  */
+    host: "smtp.hostinger.com",
+    port: 465,
+    secure: true,
     auth: {
       user: CORP_EMAIL,
       pass: PASSWORD,
@@ -33,8 +26,6 @@ export async function POST(request: NextRequest) {
     text: message,
     cc: email,
   };
-
-  console.log("mailoprions: ", mailOptions);
 
   const sendMailPromise = () =>
     new Promise<string>((resolve, reject) => {
